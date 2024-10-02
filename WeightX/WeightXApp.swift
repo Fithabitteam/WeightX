2b329e340b070cca9f3ec5821ddcd3a932d36f90
k0v0354 <k0v0354@homeoffice.wal-mart.com>
2024-10-02 19:00:43 +0530

    Initial Commit

---
+++ WeightX/WeightXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightXApp.swift
+//  WeightX
+//
+//  Created by Keerthanaa Vm on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeightXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
